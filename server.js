9511b526bba59a7c9ff8ed49ce0f310953464cc9
Rahul Patil <technologeek@Rahuls-MacBook-Pro.local>
2018-10-10 15:19:04 +0100

    updated-get-method

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ const app = express();
 const path = require('path');
 const port = 3000;
 const request = require('request');
+var router = express.Router();
+const bodyParser = require('body-parser');
 
-let result = request('http://169.254.169.254/latest/meta-data/placement/availability-zone', function (error, response, body) {
-  if (!error && response) {
-  }
-  else {
-    console.log("Error "+response)
-  }
-})
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.get("/region",function(req, res){
+    var remote = request('http://169.254.169.254/latest/meta-data/placement/availability-zone');
+    req.pipe(remote);
+    remote.pipe(res);
+  });
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.get('/region',function(req,res){
-    res.send(result);
-})
+
 
 app.listen(port, console.log("Listening on port :" + port));
