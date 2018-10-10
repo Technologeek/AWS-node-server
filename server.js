b017e7114da0485713a08a8f9b6c8837cd7bd2a7
Rahul Patil <technologeek@Rahuls-MacBook-Pro.local>
2018-10-10 14:05:27 +0100

    added-ajax-call-to-metadata

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const app = express();
 const path = require('path');
 const port = 3000;
 const request = require('request');
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-request('http://169.254.169.254/latest/meta-data/placement/availability-zone', function (error, response, body) {
+
+let result = request('https://www.google.com/', function (error, response, body) {
   if (!error && response) {
-    console.log(response.body) // Show the HTML for the Google homepage. 
   }
   else {
     console.log("Error "+response)
   }
 })
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/region',function(req,res){
+    res.send(result);
+})
+
 app.listen(port, console.log("Listening on port :" + port));
