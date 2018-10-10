0d52ef95e1dcadc0e3a9a10b51d2036392269c83
Rahul Patil <technologeek@Rahuls-MacBook-Pro.local>
2018-10-10 13:26:57 +0100

    added-curl-request-to-metadata

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const request = require('request');
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
+request('http://169.254.169.254/latest/meta-ment/availability-zone', function (error, response, body) {
+  if (!error && response) {
+    console.log(body) // Show the HTML for the Google homepage. 
+  }
+  else {
+    console.log("Error "+response)
+  }
+})
 app.listen(port, console.log("Listening on port :" + port));
