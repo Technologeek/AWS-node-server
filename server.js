aa95453752175e19579176b965dc5c10263e6bcd
Rahul Patil <technologeek@Rahuls-MacBook-Pro.local>
2018-10-10 18:12:16 +0100

    adding-styles

--- server.js
+++ server.js
@@ -3,25 +3,23 @@ const app = express();
 const path = require('path');
 const port = 3000;
 const request = require('request');
-var router = express.Router();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
     extended: true
-  }));
-  app.use(function(req, res, next) {
+}));
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  })
-  app.get("/region",function(req, res){
+})
+app.get("/region", function(req, res) {
     var remote = request('http://169.254.169.254/latest/meta-data/placement/availability-zone');
     req.pipe(remote);
     remote.pipe(res);
-  });
+});
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
 
 app.listen(port, console.log("Listening on port :" + port));
